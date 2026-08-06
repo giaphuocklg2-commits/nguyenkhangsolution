13d07710781525494ea2ea3c29ef525f1a288430
giaphuocklg2-commits <giaphuocklg2@gmail.com>
2026-08-06 12:09:34 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  async headers() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
